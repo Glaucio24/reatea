91066a4c4271f8d68674ed54b44b4e543e3fe43d
Glaucio24 <khedinhoglaudio2@gmail.com>
2025-10-12 00:47:30 -0500

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,9 @@ export default function Home() {
  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   
+   <button className="rounded-full bg-black/10 px-10 py-3 font-semibold no-underline transition hover:bg-black/20">
+          Hello World
+        </button>
     </main>
   );
 }
